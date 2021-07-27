913f8c51f937a767e75960551e2426e71d89a936
Tina <tinagrissom@yahoo.com>
2021-07-27 13:55:15 -0500

    added error boundary to user search

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -3,6 +3,7 @@ import { Fragment, useState, useEffect, Component } from "react";
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
 import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
 
 const DUMMY_USERS = [
   { id: "u1", name: "Max" },
@@ -11,20 +12,19 @@ const DUMMY_USERS = [
 ];
 
 class UserFinder extends Component {
-
   static contextType = UsersContext;
 
   constructor() {
-    super()
+    super();
     this.state = {
       filteredUsers: [],
-      searchTerm: '',
+      searchTerm: "",
     };
   }
 
   componentDidMount() {
     // Send http request....
-    this.setState({filteredUsers: this.context.users});
+    this.setState({ filteredUsers: this.context.users });
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -47,7 +47,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
